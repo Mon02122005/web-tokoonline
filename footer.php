41f0624d6dcc3417cf0c7f2a67dc1173ec7de352
Mon02122005 <mon02122004@gmail.com>
2024-05-10 01:29:51 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,29 @@
+<div class="container-fluid py-5 footer text-light">
+    <div class="container">
+        <h5 class="text-center mb-4">Hubungi Kami</h5>
+        <div class="row justify-content-center">
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="https://wa.me/+622221205074" class="no-decoration"><i class="fab fa-whatsapp fs-2"></a></i>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="https://instagram.com" class="no-decoration"><i class="fab fa-instagram fs-2"></a></i>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="https://tiktok.com" class="no-decoration"><i class="fab fa-tiktok fs-2"></a></i>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="https://facebook.com" class="no-decoration"><i class="fab fa-facebook fs-2"></a></i>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="https://youtube.com" class="no-decoration"><i class="fab fa-youtube fs-2"></a></i>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid py-3 bg-dark text-light">
+    <div class="container d-flex justify-content-between">
+        <label>&copy;2023 Cyber Electro ID</label>
+        <label>Create @Hervandi | Copy @Remon </label>
+    </div>
+</div>
---
+++ tentang-kami.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Online | Tentang Kami</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require "navbar.php" ?>
+
+    <div class="container-fluid banner2 d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center">Tentang Kami</h1>
+        </div>
+    </div>
+
+    <!-- main -->
+    <div class="container-fluid py-5">
+        <div class="container fs-5 text-center">
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea aliquam perferendis repellat in autem officiis facere, culpa atque iste maiores accusantium illum totam voluptatem sit modi ratione harum! Ipsa consequuntur est placeat hic optio excepturi quibusdam tempora eum consequatur autem aut dolorum repellendus, error aspernatur corporis animi accusamus quo. Cupiditate quo fuga obcaecati cumque, eligendi amet quis, a ullam velit quibusdam necessitatibus illum possimus in earum modi sapiente natus, veritatis hic blanditiis officia praesentium voluptate? Perspiciatis eius numquam distinctio ea fugiat, nihil amet deserunt illum omnis esse cupiditate consequatur in reiciendis vel harum ullam. Non ipsa ad voluptatibus saepe rem.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo inventore nam quisquam voluptas ea pariatur, mollitia possimus quibusdam. Voluptatum, repudiandae. Voluptas optio quibusdam incidunt. Laboriosam nisi dolore, qui rem necessitatibus in alias maiores deserunt velit omnis corrupti iure quisquam vitae, asperiores, facere ipsam eveniet reiciendis nesciunt id architecto ullam. Repellat reprehenderit exercitationem, sapiente eos commodi, unde quo quam nisi atque, illum consectetur. Quae sequi at quaerat dicta optio impedit! Facere eius molestias fugiat voluptatum ipsum natus culpa veniam laudantium explicabo!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint animi cumque blanditiis iste optio perspiciatis aut quae illum? Aperiam inventore quasi voluptatibus recusandae omnis et ut quis delectus laboriosam, facere ad soluta numquam eveniet distinctio perferendis possimus accusantium dolor dicta! Reprehenderit delectus minima exercitationem ea maiores nobis dolores perspiciatis. A, nesciunt, adipisci, nisi voluptas atque quod expedita aspernatur necessitatibus culpa exercitationem tempora aut eligendi placeat amet veritatis accusantium labore et soluta nemo est ex sed quaerat magnam blanditiis? Nihil ducimus, neque, est aut exercitationem eaque quas distinctio alias recusandae vitae, impedit quod ratione. Pariatur qui officiis corporis quidem perferendis eius, enim nam, officia unde eum quaerat consequatur ex assumenda soluta libero. Sequi iusto excepturi aliquid rerum. Molestiae officia voluptatem deleniti voluptates perspiciatis accusamus deserunt magnam quas eaque ab eligendi itaque reprehenderit ex, laudantium tempore illo dolorum delectus sint voluptatibus amet totam in quibusdam qui? Expedita iste atque aperiam quod molestias.
+            </p>
+        </div>
+    </div>
+
+    <!-- footer -->
+    <?php require "footer.php"; ?>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+</body>
+</html>
